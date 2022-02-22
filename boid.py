105a98bcbf6adfb43efb963d5f268a82a4b4fd27
Achille Huet <achillehuet@gmail.com>
2022-02-22 01:46:26 +0100

    :art: Improve linting

--- boid.py
+++ boid.py
@@ -1,16 +1,15 @@
 import numpy as np
 import pygame
 
-from utilities import rotation2D, get_distance
-
+from utilities import rotation_2d, get_distance
 from constants import SPEED, WINDOW_WIDTH, WINDOW_HEIGHT, BLUE, RADIUS
 
-SCREEN = np.array([[WINDOW_WIDTH], [WINDOW_HEIGHT]])
+SCREEN_DIMENSIONS = np.array([[WINDOW_WIDTH], [WINDOW_HEIGHT]])
 
 class Boid():
+    """Represent and define boid behavior"""
 
     def __init__(self, x, y, angle):
-        super().__init__()
         self.size = 10
 
         self.pos = np.array([[x], [y]]).astype(float)
@@ -20,21 +19,26 @@ class Boid():
 
 
     def update(self, neighbors):
+        """Use position of nearby boids to update direction, acceleration and speed of boid"""
         # update position
-        self.acc = self.cohesion(neighbors) * 0.45 + self.alignment(neighbors) * 0.5 + self.separation(neighbors) * 0.5 + self.avoidance() * 10
-
+        self.acc = (
+            self.cohesion(neighbors) * 0.5
+            + self.alignment(neighbors) * 0.5
+            + self.separation(neighbors) * 5
+            + self.avoidance() * 10
+        )
         self.vel += self.acc
         self.vel = self.vel / np.linalg.norm(self.vel) * SPEED
 
         self.pos += self.vel
-        self.pos = np.mod(self.pos, SCREEN)
+        self.pos = np.mod(self.pos, SCREEN_DIMENSIONS)
 
         # update image angle
         self.angle = np.arctan2(self.vel[1], self.vel[0])
 
 
     def draw(self, screen):
-        """Draws the boid on the given screen"""
+        """Draw the boid on the given screen"""
         # create default points for a triangle-shaped boid
         points = np.zeros((3, 2, 1))
         points[0] = np.array([[self.size],[0]])
@@ -42,7 +46,7 @@ class Boid():
         points[2] = np.array([[-self.size//2],[-self.size//2]])
 
         # rotate and translate points to their correct coordinates
-        rotated = rotation2D(self.angle) @ points
+        rotated = rotation_2d(self.angle) @ points
         rotated = rotated + self.pos
 
         # extract (x, y) coordinates and draw shape
@@ -50,7 +54,7 @@ class Boid():
         pygame.draw.polygon(screen, BLUE, new_points)
 
     def cohesion(self, neighbors):
-        """Finds the average direction to nearby boids"""
+        """Find the average direction to nearby boids"""
         direction = np.array([[0.], [0.]])
         for other in neighbors:
             direction += other.pos - self.pos
@@ -60,6 +64,7 @@ class Boid():
         return direction
 
     def alignment(self, neighbors):
+        """Find the average direction which nearby boids are facing"""
         direction = np.array([[0.], [0.]])
         for other in neighbors:
             direction += other.vel
@@ -69,33 +74,30 @@ class Boid():
         return direction
 
     def separation(self, neighbors):
+        """Give direction and magnitude of repulsion due to being close to nearby boids"""
         direction = np.array([[0.], [0.]])
         for other in neighbors:
             dist = get_distance(self.pos, other.pos)
             if dist > 0:
                 direction += (self.pos - other.pos) / dist**2
-        norm = np.linalg.norm(direction)
-        if norm > 0:
-            direction /= norm
+        # norm = np.linalg.norm(direction)
+        # if norm > 0:
+        #     direction /= norm
         return direction
 
     def avoidance(self):
+        """Compute direction allowing to avoid obstacles"""
         # avoid borders
         dist_left = self.pos[0][0]
-        dist_right = (SCREEN - self.pos)[0][0]
+        dist_right = (SCREEN_DIMENSIONS - self.pos)[0][0]
         dist_top = self.pos[1][0]
-        dist_bot = (SCREEN - self.pos)[1][0]
+        dist_bot = (SCREEN_DIMENSIONS - self.pos)[1][0]
 
-        # calculate 
+        # calculate
         dir_left = np.array([[-1], [0]]) / dist_right if dist_right < RADIUS else np.array([[0], [0]])
         dir_right = np.array([[1], [0]]) / dist_left if dist_left < RADIUS else np.array([[0], [0]])
         dir_top = np.array([[0], [-1]]) / dist_bot if dist_bot < RADIUS else np.array([[0], [0]])
         dir_bot = np.array([[0], [1]]) / dist_top if dist_top < RADIUS else np.array([[0], [0]])
 
         direction = dir_left + dir_right + dir_top + dir_bot
-        norm = np.linalg.norm(direction)
-
         return direction
-
-
-
--- utilities.py
+++ utilities.py
@@ -1,10 +1,22 @@
 import math
 import numpy as np
 
-def rotation2D(angle):
+def rotation_2d(angle):
+    """Return the matrix which rotates a vector by the given angle"""
     return np.array([[math.cos(angle), -math.sin(angle)],
                      [math.sin(angle), math.cos(angle)]])
 
 def get_distance(pos1, pos2):
+    """Compute the distance between 2 positions"""
     vec = pos2 - pos1
     return np.linalg.norm(vec)
+
+def get_all_distances(boids):
+    """Compute the distance matrix for a list of boids"""
+    distances = np.zeros((len(boids), len(boids)))
+    for idx, boid in boids.enumerate():
+        for other_id, other in boids[idx+1:].enumerate():
+            dist = get_distance(boid.pos, other.pos)
+            distances[idx, other_id] = dist
+            distances[other_id, idx] = dist
+    return distances
